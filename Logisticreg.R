38a8b1459a83447f2222799e369139b1534ad939
Khalil  Ul Rehman <30638388+krkhalil@users.noreply.github.com>
2021-02-13 11:14:31 +0500

    here is the logistic regression code and its data file

---
+++ Logisticreg.R
@@ -0,0 +1,63 @@
+
+getwd()
+
+# Read in dataset
+quality = read.csv("quality.csv")
+
+# Look at structure
+str(quality)
+
+# Table outcome
+table(quality$PoorCare)
+
+# Baseline accuracy
+98/131
+
+# Install and load caTools package
+install.packages("caTools")
+library(caTools)
+
+# Randomly split data
+set.seed(88)
+split = sample.split(quality$PoorCare, SplitRatio = 0.75)
+split
+
+# Create training and testing sets
+qualityTrain = subset(quality, split == TRUE)
+qualityTest = subset(quality, split == FALSE)
+
+# Logistic Regression Model
+QualityLog = glm(PoorCare ~ OfficeVisits + Narcotics, data=qualityTrain, family=binomial)
+summary(QualityLog)
+
+# Make predictions on training set
+predictTrain = predict(QualityLog, type="response")
+
+predictTrain
+
+# Analyze predictions
+summary(predictTrain)
+tapply(predictTrain, qualityTrain$PoorCare, mean)
+
+
+# Confusion matrix for threshold of 0.5
+table(qualityTrain$PoorCare, predictTrain > 0.5)
+
+# Sensitivity and specificity
+10/25
+70/74
+
+# Confusion matrix for threshold of 0.7
+table(qualityTrain$PoorCare, predictTrain > 0.7)
+
+# Sensitivity and specificity
+8/25
+73/74
+
+# Confusion matrix for threshold of 0.2
+table(qualityTrain$PoorCare, predictTrain > 0.2)
+
+# Sensitivity and specificity
+16/25
+54/74
+
---
+++ code.R
@@ -0,0 +1,6 @@
+iris
+colnames(iris)
+help()
+sum(iris$Sepal.Length)
+summary(iris)
+
